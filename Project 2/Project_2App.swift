ad845215df685d6199bf61e47ce5a82cf4564e8d
Daniel Yun <danielyun2018@outlook.com>
2026-02-23 21:04:04 -0500

    Initial Commit

---
+++ Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by Hyunjoon on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
